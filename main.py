2b2af678b456fad5a2d651fb84340f3f2f04c0bc
Soumyadip <nayaksoumyadip@gmail.com>
2023-12-09 23:42:12 +0530

    Added more languages

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp.json"
 st.title("AI Code generator")
 
 language = st.sidebar.selectbox(
-    "Please select your language!", ("Python", "JavaScript", "Rust", "Dart"))
+    "Please select your language!", ("C", "Python", "Java", "C++", "C#", "JavaScript", "PHP", "VB.NET", "Assembly Language", "Classic Visual Basic", "Fortran", "R", "Ruby", "Go", "Swift", "MATLAB", "Rust", "Kotlin", "Objective-C", "Scala", "Perl", "Prolog", "COBOL", "Delphi/Object Pascal", "Lua", "Haskell", "Erlang", "Groovy", "Julia", "Visual Basic", "Ada", "Eiffel", "F#", "Dart", "CLIPS", "OCaml", "Nim", "Elm", "Scratch", "TypeScript", "VHDL", "Verilog", "Pike", "Swift (Android)", "Kotlin (JVM)", "Ceylon", "Crystal", "Ballerina", "CMake"
+                                     ))
 
 problem = st.sidebar.text_area(
     "What is your problem statement?", max_chars=100)
